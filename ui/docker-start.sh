e5c804e02970cbefedae0123f23e4cea6a83e8ac
bdusek <brettdusek@gmail.com>
2023-07-20 00:57:47 -0400

    Fixing ui/docker-start differences

--- ui/docker-start.sh
+++ ui/docker-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "IS_PRODUCTION: ${IS_PRODUCTION}"
-if [ "$IS_PRODUCTION" = "true" ]; then
+if [[ "$IS_PRODUCTION" == "true" ]]; then
   echo "Running as production"
   npm run build
   npm run serve
