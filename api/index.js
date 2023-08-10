da7ea57493c6a8d346588050f4c2176dcb76cdb3
Michael Porter <michael@muddybootscode.io>
2023-08-10 15:09:46 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import { typeDefs } from "./graphql-schema.js";
 
 const driver = neo4j.driver(
   process.env.NEO4J_URI || "bolt://localhost:7687",
-  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEORJ_PASSWORD),
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD),
 );
 
 const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
