c57bd5de3662d8c3ccdc721eabfeaf80c4ec3ac9
muddybootscode <Michael@muddybootscode.io>
2023-08-12 13:21:53 -0500

    Refactor: removed console.logs

--- api/index.js
+++ api/index.js
@@ -5,12 +5,6 @@ import neo4j from "neo4j-driver";
 import { typeDefs } from "./graphql-schema.js";
 import "dotenv/config";
 
-console.log(
-  process.env.NEO4J_URI,
-  process.env.NEO4J_USER,
-  process.env.NEO4J_PASSWORD,
-);
-
 const driver = neo4j.driver(
   process.env.NEO4J_URI || "bolt://localhost:7687",
   neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD),
